23ee7254e67733938542836e198e31014b40d198
Luiz Filipe de Lima Gomes Santos <luizfilipelgs@gmail.com>
2023-09-10 12:11:11 -0300

    feat: inputValidation

---
+++ Praticas-de-Cybersegurança/inputValidation.js
@@ -0,0 +1,25 @@
+// Carregue o módulo readline do Node.js para interagir com o console.
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+// Solicite ao usuário inserir algum texto.
+rl.question('Digite algo (tente usar caracteres especiais como < ou >): ', (input) => {
+    
+    // Função para validar a entrada do usuário.
+    function validateInput(data) {
+        // Substitua quaisquer instâncias de < ou > para evitar injeção maliciosa, por exemplo.
+        const sanitized = data.replace(/<|>/g, '');
+        return sanitized;
+    }
+
+    const sanitizedInput = validateInput(input);
+
+    console.log(`Entrada original: ${input}`);
+    console.log(`Entrada após validação: ${sanitizedInput}`);
+    
+    rl.close();
+});
